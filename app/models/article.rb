a105050487eb15bb7bb09bad1aed18c0c59159bc
Roger Oba <rogerluan.oba@gmail.com>
2021-04-06 02:08:08 -0300

    Delete previous rails blog app and create a new one, going through Section 8.2 of https://guides.rubyonrails.org/getting_started.html

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,11 @@
 class Article < ApplicationRecord
+  has_many :comments
+
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 10 }
+
+  def average_reading_time
+    # 200 is the average number of words an average person reads per minute
+    body.split(/\W+/).length / 200
+  end
 end
